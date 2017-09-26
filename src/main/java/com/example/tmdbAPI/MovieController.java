7e369e2f69788b5b933a60a8a91352d4eeae4d54
EliGarcia <garciaelisha93@yahoo.com>
2017-09-26 14:04:53 -0500

    added @Requestmapping for controller

--- src/main/java/com/example/tmdbAPI/MovieController.java
+++ src/main/java/com/example/tmdbAPI/MovieController.java
@@ -2,9 +2,36 @@ package com.example.tmdbAPI;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Controller
 public class MovieController {
 
     static final String API_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=be2a38521a7859c95e2d73c48786e4bb";
+
+
+    @RequestMapping("/")public String home(Model model){
+        return "home";
+    }
+
+    @RequestMapping("/medium-popular-long-name")public String mediumPopularLongName(Model model){
+        return "medium-popular-long-name";
+    }
+
+    @RequestMapping("now-playing")public String nowPlaying(Model model){
+        return "now-playing";
+    }
+
+
+    public static List<Movie> getMovies(String route){
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getForObject(API_URL, ResultsPage.class);
+
+    }
+
 }
