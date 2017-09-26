da920bfd25971ab014be6cebf9a9c241fc0d616a
EliGarcia <garciaelisha93@yahoo.com>
2017-09-26 13:35:06 -0500

    first commit

---
+++ src/main/java/com/example/tmdbAPI/TmdbApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.tmdbAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmdbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmdbApiApplication.class, args);
+	}
+}
