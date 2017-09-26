8a199c5a013e650c54bf7cef54bc33c4451eb243
EliGarcia <garciaelisha93@yahoo.com>
2017-09-26 13:49:26 -0500

    added MovieController and able to use api via postman

---
+++ src/main/java/com/example/tmdbAPI/MovieController.java
@@ -0,0 +1,10 @@
+package com.example.tmdbAPI;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MovieController {
+
+    static final String API_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=be2a38521a7859c95e2d73c48786e4bb";
+}
